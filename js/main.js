c2c81fc33fcc66c81a2b6431f17f69fb854ccb41
Christina Paik <christinapaik@gmail.com>
2017-11-30 18:55:05 -0500

    add events page

--- js/main.js
+++ js/main.js
@@ -6,3 +6,33 @@ $(document).ready(function(){
       autoplayTimeout: 3000
   });
 });
+
+
+
+	$(document).ready(function() {
+
+		$('#calendar').fullCalendar({
+            events: [
+        {
+            title  : 'Hair Sample Show',
+            start  : '2017-12-05'
+        },
+        {
+            title  : 'Makeup Session',
+            start  : '2017-12-01',
+            end    : '2017-12-02'
+        },
+        {
+            title  : 'Holiday Sale',
+            start  : '2017-12-13T12:30:00',
+            allDay : false // will make the time show
+        },
+        {
+            title  : 'Last Minute Makeup Sessions',
+            start  : '2017-12-25T13:30:00',
+            allDay : false // will make the time show
+        }
+    ]
+		});
+
+	});
